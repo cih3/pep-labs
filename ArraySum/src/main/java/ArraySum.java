51e205b3ea5126d59c419b981cab140b0c5bce2f
Caleb Harris <calebiharris@icloud.com>
2023-01-06 03:33:59 +0000

    Saved progress on ArraySum 2023-01-06 03:33:59 +0000

--- ArraySum/src/main/java/ArraySum.java
+++ ArraySum/src/main/java/ArraySum.java
@@ -7,7 +7,9 @@ public class ArraySum {
      * @return the sum of all values in arr.
      */
     public int sum(int[] arr){
-
-        return 0;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+          }
+        return (arr[i]);
     }
 }
