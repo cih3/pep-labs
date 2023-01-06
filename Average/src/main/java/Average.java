74a6e0cbd79fa8053e9e9d111099872911b3b37d
Caleb Harris <calebiharris@icloud.com>
2023-01-06 04:18:32 +0000

    Saved progress on Average 2023-01-06 04:18:32 +0000

--- Average/src/main/java/Average.java
+++ Average/src/main/java/Average.java
@@ -18,6 +18,12 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+         double total=0;
+        int length= nums.length;
+        for(int i = 0; i < nums.length; i++ ){
+            total=total+nums[i];
+        }
+        double average=total/length;
+        return average;
     }
 }
