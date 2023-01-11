8685147579bae40ba328c17e3f40698effe2f757
Caleb Harris <calebiharris@icloud.com>
2023-01-11 06:46:13 +0000

    Saved progress on GetMax 2023-01-11 06:46:13 +0000

--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -10,7 +10,7 @@ public class GetMax {
         
     int max = arr[0];
         
-        for(int i=0; i< arr.length; i++){
+        for(int i=1; i< arr.length; i++){
             System.out.println(arr[i]);
         }
         return arr[max(arr)];
