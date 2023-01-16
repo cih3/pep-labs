a29a9b33ac7dca2e654d2ce8d9c0e11d9e38926a
Caleb Harris <calebiharris@icloud.com>
2023-01-16 09:50:43 +0000

    Saved progress on GroupBy 2023-01-16 09:50:43 +0000

--- GroupBy/problem1.sql
+++ GroupBy/problem1.sql
@@ -1,14 +1,2 @@
---SELECT COUNT(song) album, FROM song GROUP BY artist;
-
-/*SELECT COUNT(CustomerID), Country
-FROM Customers
-GROUP BY Country;*/
-
---SELECT Shippers.ShipperName, COUNT(song) FROM Orders GROUP BY ShipperName;
-
-
-
---SELECT COUNT(artist) song FROM song GROUP BY artist;
---SELECT COUNT(song) FROM song GROUP BY artist;
---SELECT artist COUNT(song) FROM song GROUP BY artist;
-SELECT artist COUNT(songs) AS song FROM song GROUP BY artist;
+SELECT artist, COUNT(song) FROM song GROUP BY artist;
+--SELECT COUNT(song), artist FROM song GROUP BY artist;
