e9c443add79d6a760b27953a4e996d3b96fb5853
Caleb Harris <calebiharris@icloud.com>
2023-01-19 00:28:08 +0000

    Saved progress on CreatingEndpoints 2023-01-19 00:28:08 +0000

--- CreatingEndpoints/src/main/java/com/revature/JavalinSingleton.java
+++ CreatingEndpoints/src/main/java/com/revature/JavalinSingleton.java
@@ -15,8 +15,8 @@ public class JavalinSingleton {
         Javalin app = Javalin.create();
         
         //write endpoint here
-
+        app.get("/hello", ctx -> {ctx.result("Hello World");});
         return app;
-    }
+    
     
 }
