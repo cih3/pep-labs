169ab8671ee9301a139df41fa18bc363e18e62f0
Caleb Harris <calebiharris@icloud.com>
2023-01-20 19:16:44 +0000

    Saved progress on FlightTracker 2023-01-20 19:16:44 +0000

--- IsSorted/src/main/java/IsSorted.java
+++ IsSorted/src/main/java/IsSorted.java
@@ -7,8 +7,12 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        for(int i= 0; i < arr.length; i++){
-            if(arr[i] == arr[i]) return true;
+        boolean ball = false;
+        for(int i= 0; i <= arr.length; i++){
+            if(arr[i] > arr[i+1]){
+                return ball;
+                
+         
 }
             return false;
 }
