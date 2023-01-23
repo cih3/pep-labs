5bc863bbe6f1180d635b65ed846faf22b2a27f41
Caleb Harris <calebiharris@icloud.com>
2023-01-23 17:29:01 +0000

    Saved progress on InnerJoin 2023-01-23 17:29:01 +0000

--- InnerJoin/problem1.sql
+++ InnerJoin/problem1.sql
@@ -1,3 +1,1 @@
-SELECT student.student_name FROM class INNER JOIN student 
-ON class.id = students.class_title 
-WHERE class.teacher_name = student.class_title;
+SELECT student.id, student.student_name FROM student INNER JOIN class ON class.class_title = student.class_title WHERE class.teacher_name ='Ms. Lovelace';
