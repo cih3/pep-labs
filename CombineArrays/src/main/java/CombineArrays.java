0c79da77b05853036a53d5f624958c02191af943
Caleb Harris <calebiharris@icloud.com>
2023-01-26 22:20:06 +0000

    Saved progress on CombineArrays 2023-01-26 22:20:06 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+
+import com.azul.crs.client.Result;
 
 public class CombineArrays {
     /**
@@ -8,7 +11,21 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        //int a1 = arr1.length;
+        //int a2 = arr2.length;
+        int[] result = new int[arr1.length + arr2.length];
+        int c=0;
+        for( int i = 0; i<arr1.length;i++){
+            result[i]= arr1[i];
+            c++;
+        }
+        for( int j = 0; j < arr2.length;j++){
+            result[c++]= arr2[j];
+        }
+        for( int i = 0; i < result.length; i++){
+            //System.out.println(result[i]+ " ");;
+        }
 
-        return null;
+        return result;
     }
 }
