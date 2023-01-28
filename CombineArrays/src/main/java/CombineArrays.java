8f5ab57c1d5c971cd23a73263add3cd3463016bc
Caleb Harris <calebiharris@icloud.com>
2023-01-28 00:19:08 +0000

    Saved progress on ContainsDuplicates 2023-01-28 00:19:08 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -11,8 +11,6 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-        //int a1 = arr1.length;
-        //int a2 = arr2.length;
         int[] result = new int[arr1.length + arr2.length];
         int c=0;
         for( int i = 0; i<arr1.length;i++){
@@ -23,7 +21,7 @@ public class CombineArrays {
             result[c++]= arr2[j];
         }
         for( int i = 0; i < result.length; i++){
-            //System.out.println(result[i]+ " ");;
+           
         }
 
         return result;
