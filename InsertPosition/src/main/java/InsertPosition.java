952e22c39a0213518ee40571421ae05f41e810e2
Caleb Harris <calebiharris@icloud.com>
2023-02-10 01:58:07 +0000

    Saved progress on InsertPosition 2023-02-10 01:58:07 +0000

--- InsertPosition/src/main/java/InsertPosition.java
+++ InsertPosition/src/main/java/InsertPosition.java
@@ -15,6 +15,30 @@ public class InsertPosition {
      * @return return the index that a number should be inserted into a sorted array.
      */
     public int searchInsert(int[] nums, int target) {
-        return -1;
+        int start = 0;
+        int end = nums.length - 1;
+        
+         // Traverse an array
+        while( start <= end ) {
+            
+            int mid = (start + end) / 2;
+            
+            //if target value found.
+            if(nums[mid] == target) {
+                return mid;
+            }
+            
+            // If target value is greater then mid elements's value
+            else if (target > nums[mid]) {
+                start = mid + 1;
+            }
+            
+            //otherwise target value is less, 
+             else {
+                end = mid - 1;
+            }
+        }
+        // Return the insertion position
+        return end + 1;
     }
 }
